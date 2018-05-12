db8f52d8cacf0dee90b55802ce53a031b0766f13
jeeve <jvjulien@free.fr>
2018-05-12 16:56:50 +0200

    description

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
+      <title>Lac de Léry-Poses</title>
+      <META NAME="Description" CONTENT="Le lac de Léry Poses avec webcam, archives, météo en temps réel et prévisions, lien vers l'association WLPA."/>
 <meta http-equiv="refresh" content="0; URL=http://jvjulien.free.fr/sensations/lac-lery-poses.php" />
 </head>
 <body>
